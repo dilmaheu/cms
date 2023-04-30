d31ba28ed72192d4b22405e4f1e98ba3fb56f797
jurjendevries <jurjen@jurjendevries.com>
2023-04-30 23:47:58 +0000

    get server configs from env vars

--- config/server.js
+++ config/server.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = ({ env }) => ({
-  host: env('HOST', 'localhost'),
-  port: env.int('PORT', 1337),
+  host: env('HOST'),
+  port: env('PORT'),
   app: {
     keys: env.array('APP_KEYS'),
   },
   emitErrors: false,
-  url: env('PUBLIC_URL', 'https://cms.dilmahtea.me'),
+  url: env('PUBLIC_URL'),
   proxy: env.bool('IS_PROXIED', true),
   webhooks: {
     // Add this to not receive populated relations in webhooks
