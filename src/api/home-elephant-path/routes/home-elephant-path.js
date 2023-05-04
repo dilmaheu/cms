3c9fc4722a2535ed1f23399b232f729b97f53db0
jurjendevries <jurjen@jurjendevries.com>
2023-05-04 09:44:34 +0000

    added updated schemas and configs

---
+++ src/api/home-elephant-path/routes/home-elephant-path.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-elephant-path router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-elephant-path.home-elephant-path');
---
+++ src/api/home-elephant-path/services/home-elephant-path.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-elephant-path service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::home-elephant-path.home-elephant-path');
