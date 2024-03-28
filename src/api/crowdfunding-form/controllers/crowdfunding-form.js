6605a0dd13c5e4344088f48805169721c00a19d3
dilmaheurope <sales.europe@dilmahtea.com>
2024-03-28 08:58:27 +0000

    migration update

--- src/api/crowdfunding-form/controllers/crowdfunding-form.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  crowdfunding-form controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::crowdfunding-form.crowdfunding-form');
--- src/api/crowdfunding-form/routes/crowdfunding-form.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * crowdfunding-form router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::crowdfunding-form.crowdfunding-form');
